ecdcd0d86dcbe19b5f482fe63ea204f14e941447
Confidence Okoghenun <confidenceboi@gmail.com>
2020-07-13 18:06:58 +0100

    feat: Upgrades single download

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -3,25 +3,31 @@ import { jsx } from '@emotion/core';
 import mq from './MediaQuery';
 
 const DownloadBtn = ({ callback }) => (
-  <svg
+  <span
     onClick={callback}
     className='single_download_btn'
-    viewBox='0 0 24 20'
     css={{
-      fill: 'rgb(29, 161, 242)',
-      margin: '-10px 0 0 10px',
       cursor: 'pointer',
+      margin: '-10px 0 0 10px',
     }}
   >
-    <g>
-      <path
-        d='M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z'
-        transform='scale (1, -1)'
-        transform-origin='center'
-      ></path>
-      <path d='M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z'></path>
-    </g>
-  </svg>
+    <svg
+      viewBox='0 0 24 20'
+      css={{
+        pointerEvents: 'none',
+        fill: 'rgb(29, 161, 242)',
+      }}
+    >
+      <g>
+        <path
+          d='M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z'
+          transform='scale (1, -1)'
+          transform-origin='center'
+        ></path>
+        <path d='M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z'></path>
+      </g>
+    </svg>
+  </span>
 );
 
 const SongItem = ({
@@ -99,14 +105,11 @@ const SongItem = ({
           <time>{duration}</time>
         </div>
         {url ? (
-          <a
-            className='single_download_btn'
-            css={{ all: 'unset' }}
-            href={url}
-            download
-          >
-            <DownloadBtn callback={() => {}} />
-          </a>
+          <DownloadBtn
+            callback={() => {
+              downloadOne(url);
+            }}
+          />
         ) : (
           <DownloadBtn
             callback={() => {
