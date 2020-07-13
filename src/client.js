11836a086d751ffbd40709e1c689452c9c56c676
Confidence Okoghenun <confidenceboi@gmail.com>
2020-07-13 04:49:59 +0100

    fix: Fixes env vars

--- src/client.js
+++ src/client.js
@@ -3,11 +3,6 @@ import { HttpLink } from 'apollo-link-http';
 import { ApolloClient } from 'apollo-client';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import  dotenv  from  'dotenv' 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-
 
 const delay = setContext(
   (request) =>
@@ -21,7 +16,7 @@ const delay = setContext(
 
 const cache = new InMemoryCache();
 const http = new HttpLink({
-  uri: process.env.API,
+  uri: process.env.REACT_APP_API,
 });
 
 const link = ApolloLink.from([delay, http]);
